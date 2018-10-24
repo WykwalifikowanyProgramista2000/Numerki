8aaf826c27719cc87d8554828b8916f489e2a31c
Wojciech Dziuba <wdziuba@student.agh.edu.pl>
2018-10-24 19:00:59 +0200

    hops postępy w lab3

--- spyder/lab3.py
+++ spyder/lab3.py
@@ -10,18 +10,15 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def GG(n: int)
+
 def Gp(n: int)->list:
     temp = []
     for p in range(1, n+1):
-        temp.append((1/p)*((10**p)*(1+p*np.round(pi, 15)*(10**(-p)))-(10**p)))
+        temp.append((1/p)*((10**p)*(1+p*np.round(math.pi, 15)*(10**(-p)))-(10**p)))
     return np.array(temp)
 
 wyn = Gp(15)
-print(Gp(15), "XXX", len(wyn))
-
-
-#%% Zadanie 3
+#print(wyn, "XXX", len(wyn))
 
 bez = []
 wzg = []
@@ -30,21 +27,74 @@ def fbez(wyn: list)->list:
     temp = []
     for i in range(len(wyn)):
         temp.append(math.fabs(wyn[i] - math.pi))
-    return temp
+    return list(temp)
 
 
 def fwzg(wyn: list)->list:
     temp = []
     for i in range(len(wyn)):
         temp.append(math.fabs((wyn[i] - math.pi)/math.pi))
-    return temp
+    return list(temp)
+
 
 bez = fbez(wyn)
 wzg = fwzg(wyn)
 
-x = np.linspace(0, 14)
-plt.plot(x, list(bez))
-plt.semilogy(y)
-plt.show()
+#print("bez:", bez, "\nwzg:", wzg)
+
+
+n = np.linspace(0, 14, 15)
+plt.semilogy(n, list(bez))
+plt.legend("bezwzględny")
+plt.xlim(left = 0)
+plt.xlim(right = 15)
+plt.xlabel("n")
+plt.ylabel("błąd")
+plt.semilogy(n, list(wzg))
+plt.legend("względny")
+plt.title("wykres błędów bezwzględnych i względnych")
+
+
+#%% Zadanie 3
+
+import math
+
+def myE(stop: int)->float:
+    temp = 0
+    for n in range(1, stop+1):
+        temp += 1/math.factorial(n)
+    return temp
+
+bez5 = math.fabs(math.e - myE(5))
+bez10 = math.fabs(math.e - myE(10))
+wzg5 = math.fabs((math.e - myE(5))/math.e)
+wzg10 = math.fabs((math.e - myE(10))/math.e)
+
+print("Błędy dla n = 5 to:\n___Bezwzględny:", bez5, "\n___Względny:", wzg5)
+print("Błędy dla n = 10 to:\n___Bezwzględny:", bez10, "\n___Względny:", wzg10)
+
+
+#%% Zadanie 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
